942626b3797ec8b4e3cb17cd0eb691a87febbda6
jota <aleph0528@gmail.com>
2022-03-27 22:46:42 +0900

    firstcommit

---
+++ content.js
@@ -0,0 +1,42 @@
+
+
+window.addEventListener('load', () => {  
+  // オブザーバーの作成
+  const observer = new MutationObserver(records => {
+    // 変化が発生したときの処理を記述
+    chrome.storage.local.get('users', (data) => {
+
+    users = data.users;
+    
+    // 空なら
+    if(users.length == 0) return;
+      users.forEach(user => {
+        hideBlockUserArticles(user);
+      })
+    });
+
+  });
+  
+  // 監視の開始
+  observer.observe(document, {
+    childList: true,
+    subtree: true
+  })
+});
+
+// 指定のユーザの記事を非表示
+const hideBlockUserArticles = (userName) => {
+  let userLinks = document.querySelectorAll(`a[href="/users/${userName}"]`);
+  if(userLinks == undefined) return;
+
+  userLinks.forEach(userLink => {
+    if(userLink.textContent != userName) return;
+
+    let article = userLink.closest("article");
+    if(article == undefined) return;
+    if(article.style.display == 'none') return;
+
+    article.style.display = 'none';
+    console.log(`${userName}の質問を非表示にしました`);
+  });
+}
---
+++ popup.js
@@ -0,0 +1,78 @@
+// 拡張機能ポップアップの要素
+const $btn = document.querySelector('#btn');
+const $allDelete = document.querySelector('#allDelete');
+const $list = document.querySelector('#list');
+
+window.addEventListener('load', () => {
+  showUser();
+});
+
+// clickイベント
+$btn.addEventListener('click', () => {
+  const $form = document.querySelector('form');
+  let value = $form.name.value;
+  // 空文字か文字数制限外の時
+  if(value == null || value == '' || value.length > 200) {
+    var warn_message = '文字を入力してください';
+    window.alert(warn_message);
+    return;
+  }
+
+  chrome.storage.local.get('users', (data) => {
+    let usersData;
+    users = data.users;
+    usersData = users;
+    
+    // 空なら
+    if(users.length == 0) {
+      usersData = [];
+    }
+
+    // 追加
+    usersData.push(value);
+    addUserListElement(value);
+  
+    // 保存
+    chrome.storage.local.set({'users': usersData}, () => {
+      console.log('set');
+    });
+  });
+});
+
+$allDelete.addEventListener('click', () => {
+  chrome.storage.local.set({'users': []}, () => {
+    $list.innerHTML = '';
+  });
+});
+
+
+const showUser = () => {
+  chrome.storage.local.get('users', (data) => {
+    let usersData;
+    users = data.users;
+    usersData = users;
+    // 空なら
+    if(users.length == 0) {
+      usersData = [];
+    }
+
+    if(usersData) {
+      usersData.forEach(user => {
+        let li = createUserLiElement(user);
+        $list.append(li);
+      });
+    }
+  });
+}
+
+const addUserListElement = (name) => {
+  let $li = createUserLiElement(name);
+  $list.append($li);
+}
+
+// liタグのElemntを作成
+const createUserLiElement = (name) => {
+  let $li = document.createElement('li');
+  $li.textContent = name;
+  return $li;
+}
