213a84da601e276b8c87462308bdcffea67d5de1
jota <aleph0528@gmail.com>
2022-04-12 01:01:18 +0900

    update: 変更された名前を指定しても非表示に出来るようにした

--- content.js
+++ content.js
@@ -28,13 +28,25 @@ window.addEventListener('load', () => {
 // 指定のユーザの記事を非表示
 const hideBlockUserArticles = (userName) => {
   let userLinks = document.querySelectorAll(`a[href="/users/${userName}"]`);
-  if(userLinks == undefined) return;
 
-  // 削除処理
-  userLinks.forEach(userLink => {
-    // 非表示
-    artcleDisplayNoneByName(userName, userLink);
-  });
+  // urlをキーに削除処理
+  if(userLinks) {
+    userLinks.forEach(userLink => {
+      // 非表示
+      artcleDisplayNoneByName(userName, userLink);
+    });
+  }
+
+  // 表示名を変更したユーザの質問の削除処理
+  const imgs = document.querySelectorAll('img');
+  if(imgs) {
+    imgs.forEach(img => {
+      let alt = img.getAttribute('alt');
+  
+      if(alt != userName) return;
+      artcleDisplayNoneByName(userName, img);
+    });
+  }
 }
 
 const artcleDisplayNoneByName = (name, targetElement) => {
